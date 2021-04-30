d9557d5012a6e449f5c0fc0aaceaab81c39494d4
Filip berglund <ryerzs@hotmail.com>
2021-04-30 15:35:02 +0200

    Basic structure of node class

--- node.py
+++ node.py
@@ -2,4 +2,16 @@
 
 class Node:
     def __init__(self):
+        self.adjNodes = []
+        self.edges = []
         pass
+    def drawEdge(self):
+        for node in self.adjNodes:
+            self.edgeDrawn(node,True)
+            node.self.edgeDrawn(self,True)
+    def edgeDrawn(self, node, b):
+        for e in self.edges:
+            if e[0] == node:
+                e[1] = b
+
+        
