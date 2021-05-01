2053502f68aa450a8fb63918f7895b4dfada7148
Filip berglund <ryerzs@hotmail.com>
2021-05-01 21:57:04 +0200

    Transformation matrix and transformation

--- cube.py
+++ cube.py
@@ -36,16 +36,7 @@ class Cube():
         for i in range(len(pos)):
             self.nodes[i].setPos(pos[i])
 
-    def rotate(self, axis, deg):
-        theta = deg*math.pi/180
-        c = math.cos(theta)
-        s = math.sin(theta)
-        if axis == 'x':
-            A = [[1, 0, 0], [0, c, -s], [0, s, c]]
-        elif axis == 'y':
-            A = [[c, 0, s], [0, 1, 0], [-s, 0, c]]
-        elif axis == 'z':
-            A = [[c, -s, 0], [s, c, 0], [0, 0, 1]]
+    def transform(self, A):
         pos = self.getPos()
         pos = transpose(pos)
         pos = matmul(A,pos)
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from cube import *
 
+def rotationMatrix(axis, deg):
+    theta = deg*math.pi/180
+    c = math.cos(theta)
+    s = math.sin(theta)
+    if axis == 'x':
+        A = [[1, 0, 0], [0, c, -s], [0, s, c]]
+    elif axis == 'y':
+        A = [[c, 0, s], [0, 1, 0], [-s, 0, c]]
+    elif axis == 'z':
+        A = [[c, -s, 0], [s, c, 0], [0, 0, 1]]
+    return A
+
+def transformationMatrix(A,B):
+    return matmul(A,B)
+
 if __name__ == '__main__':
     cube = Cube(1)
-    cube.rotate('z', 90)
+    A = rotationMatrix('z',90)
+    cube.transform(A)
     print(cube.getPos())
     
